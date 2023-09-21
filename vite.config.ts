a166c95d7b58e17f23919f8673ec05f4c41c148f
Michael Bauer <mbauer@bastiansolutions.com>
2023-09-21 11:03:51 -0600

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
-import typescript2 from 'rollup-plugin-typescript2';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,22 +10,6 @@ export default defineConfig({
     vue(),
     vuetify({
       autoImport: true
-    }),
-    dts({
-      insertTypesEntry: true
-    }),
-    typescript2({
-      check: false,
-      include: ['src/components/**/*.vue'],
-      tsconfigOverride: {
-        compilerOptions: {
-          outDir: 'dist',
-          sourceMap: true,
-          declaration: true,
-          declarationMap: true
-        }
-      },
-      exclude: ['vite.config.ts']
     })
   ],
   resolve: {
