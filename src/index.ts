3d12e3d85c2a2e9996fc2bd72291328d5f1e7219
Michael Bauer <mbauer@bastiansolutions.com>
2023-09-21 09:36:57 -0600

    change component lib name

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import * as components from './components';
 import type { App } from 'vue';
 
-export const BastianTouchComponentLib = {
-  install: (app: App) =>{
+export const ComponentLib = {
+  install: (app: App) => {
     Object.entries(components).forEach(([componentName, component]) => {
       console.debug(componentName);
       app.component(componentName, component);
